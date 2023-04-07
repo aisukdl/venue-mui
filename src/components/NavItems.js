bd18bf5790f808ac2995695f268b4e6078285e22
aisukdl <pk_aisu@hotmail.co.th>
2023-04-07 13:59:43 +0700

    add link to navbar

--- src/components/NavItems.js
+++ src/components/NavItems.js
@@ -6,71 +6,84 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import WebIcon from '@mui/icons-material/Web';
 import LockIcon from '@mui/icons-material/Lock';
 import DoNotDisturbAltIcon from '@mui/icons-material/DoNotDisturbAlt';
+import { Link } from "react-router-dom";
 
 export const NavItems = () => { 
     return(
         <Box>
             <List>
+                <Link to='/dashboard'>
+                    <ListItem>
+                        <ListItemButton selected={true}>
+                            <ListItemIcon>
+                                <TimelineIcon />
+                            </ListItemIcon>
+                            <ListItemText>
+                                Dashboard
+                            </ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                </Link>
+                <Link to='#'>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <AccountBoxIcon />
+                            </ListItemIcon>
+                            <ListItemText>
+                                User
+                            </ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                </Link>
+                <Link to='/products'>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ShoppingCartIcon />
+                            </ListItemIcon>
+                            <ListItemText>
+                                Product
+                            </ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                </Link>
+               <Link to='#'>
                 <ListItem>
-                    <ListItemButton selected={true}>
-                        <ListItemIcon>
-                            <TimelineIcon />
-                        </ListItemIcon>
-                        <ListItemText>
-                            Dashboard
-                        </ListItemText>
-                    </ListItemButton>
-                </ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <WebIcon />
+                            </ListItemIcon>
+                            <ListItemText>
+                                Blog
+                            </ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+               </Link>
+               <Link to='/login'>
                 <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <AccountBoxIcon />
-                        </ListItemIcon>
-                        <ListItemText>
-                            User
-                        </ListItemText>
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <ShoppingCartIcon />
-                        </ListItemIcon>
-                        <ListItemText>
-                            Product
-                        </ListItemText>
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <WebIcon />
-                        </ListItemIcon>
-                        <ListItemText>
-                            Blog
-                        </ListItemText>
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <LockIcon />
-                        </ListItemIcon>
-                        <ListItemText>
-                            Login
-                        </ListItemText>
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <DoNotDisturbAltIcon />
-                        </ListItemIcon>
-                        <ListItemText>
-                            Not Found
-                        </ListItemText>
-                    </ListItemButton>
-                </ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <LockIcon />
+                            </ListItemIcon>
+                            <ListItemText>
+                                Login
+                            </ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+               </Link>
+                <Link to='#'>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <DoNotDisturbAltIcon />
+                            </ListItemIcon>
+                            <ListItemText>
+                                Not Found
+                            </ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                </Link>
             </List>
         </Box>
 )}
